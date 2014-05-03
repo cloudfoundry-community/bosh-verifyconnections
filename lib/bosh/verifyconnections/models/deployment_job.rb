e5555dfa2fd169c3853ada4858213acfee2b842c
Dr Nic Williams <drnicwilliams@gmail.com>
2014-05-03 10:41:23 -0700

    Initial DeploymentJob class fleshed out

--- lib/bosh/verifyconnections/models/deployment_job.rb
+++ lib/bosh/verifyconnections/models/deployment_job.rb
@@ -1,21 +1,64 @@
 class Bosh::VerifyConnections::DeploymentJob
+  class DnsInvalidCanonicalName < StandardError; end
+
   def initialize(job_from_manifest)
     @from_manifest = job_from_manifest
+  end
+
+  def job_name
+    @from_manifest["name"]
   end
 
   def instances
     @from_manifest["instances"]
   end
 
-  def static_ips_assigned
-    []
+  def networks
+    @from_manifest["networks"]
   end
 
-  def dns_offered
-    []
+  def static_ips_assigned
+    networks.inject([]) do |ips, network|
+      if network["static_ips"]
+        ips.push(*network["static_ips"])
+      end
+      ips
+    end
+  end
+
+  def dns_offered(deployment_name, dns_suffix)
+    deployment_name = dns_canonical(deployment_name)
+    dns_suffix = dns_canonical(dns_suffix)
+    job_name = dns_canonical(self.job_name)
+    networks.inject([]) do |hostnames, network|
+      network_name = dns_canonical(network["name"])
+      0.upto(instances-1) do |n|
+        hostnames << [n, job_name, network_name, deployment_name, dns_suffix].join(".")
+      end
+      hostnames
+    end
   end
 
   def job_properties
-    {}
+    @from_manifest["properties"] || {}
   end
+
+  private
+    def dns_canonical(string)
+      unless string
+        raise DnsInvalidCanonicalName, "Must provide a string"
+      end
+      # a-z, 0-9, -, case insensitive, and must start with a letter
+      string = string.downcase.gsub(/_/, "-").gsub(/[^a-z0-9-]/, "")
+      if string =~ /^(\d|-)/
+        raise DnsInvalidCanonicalName,
+              "Invalid DNS canonical name `#{string}', must begin with a letter"
+      end
+      if string =~ /-$/
+        raise DnsInvalidCanonicalName,
+              "Invalid DNS canonical name `#{string}', can't end with a hyphen"
+      end
+      string
+    end
+
 end
--- spec/models/deployment_job_spec.rb
+++ spec/models/deployment_job_spec.rb
@@ -2,23 +2,32 @@ describe Bosh::VerifyConnections::DeploymentJob do
   context "static_ips" do
     subject do
       Bosh::VerifyConnections::DeploymentJob.new({
-        "name" => "service",
+        "name" => "service_name",
         "instances" => 1,
         "templates" => [{
           "name" => "postgres"
         }],
         "networks" => [
           {
-            "name" => "cf1",
+            "name" => "internal_network",
             "default" => ["dns", "gateway"]
           },
           {
             "name" => "floating",
             "static_ips" => ["10.244.0.6"]
           }
-        ]
+        ],
+        "properties" => {
+          "port" => 3333
+        }
       })
     end
     it { expect(subject.instances).to eq(1) }
+    it { expect(subject.static_ips_assigned).to eq(["10.244.0.6"])}
+    it { expect(subject.dns_offered("my_deployment", "suffix")).to eq([
+      "0.service-name.internal-network.my-deployment.suffix",
+      "0.service-name.floating.my-deployment.suffix"
+    ])}
+    it { expect(subject.job_properties).to eq({"port" => 3333}) }
   end
 end
